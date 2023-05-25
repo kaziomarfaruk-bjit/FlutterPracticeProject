0981cdcbabb69070e3fdc30afa6a79524d7cae4e
Kazi Omar Faruk <kazi.faruk@bjitgroup.com>
2023-05-25 10:03:11 +0600

    day 1 commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo', 
-      home: Material(child: Center(child: Text('Hello Flutter'),),),
+      title: 'Flutter Demo',
+      home: Material(
+        child: Center(
+          child: Text('Hello Flutter'),
+        ),
+      ),
     );
   }
 }
