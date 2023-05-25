a577ee97360cf20e150bba88be5fd73a75a7eb8f
Kazi Omar Faruk <kazi.faruk@bjitgroup.com>
2023-05-25 10:01:15 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo', 
+      home: Material(child: Center(child: Text('Hello Flutter'),),),
+    );
+  }
+}
